df04f9340c159e7759e6706d4ee27795f83fcafd
Shahzod <shahzodwill@gmail.com>
2023-11-08 19:59:46 +0100

    added responsiveness to list html

--- js/media.js
+++ js/media.js
@@ -26,3 +26,20 @@ menuLinks.forEach(function (el) {
     document.body.classList.remove('stop-scroll');
   })
 })
+
+// filter button in list html
+
+let filterBtn = document.querySelector('.filter__btn');
+let filterBox = document.querySelector('.filter');
+
+filterBtn.addEventListener('click', function(){
+    filterBox.classList.toggle('is-active');
+})
+
+document.addEventListener('click', function (event) {
+    // Check if the click target is not the filter button or the filter box.
+    if (event.target !== filterBtn && !filterBox.contains(event.target)) {
+      // Click occurred outside of the filter box, so hide it.
+      filterBox.classList.remove('is-active');
+    }
+});
