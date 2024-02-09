526fb9a99a980a71d6242099ded489ac25c8d868
Shahzod <shahzodwill@gmail.com>
2024-02-09 17:56:17 +0100

    display fetched products on homepage

---
+++ js/fetch.js
@@ -0,0 +1,21 @@
+async function fetchData(url) {
+    try {
+        const data = await fetch(url);
+	    const json = await data.json();
+	    return json;
+    } catch (error) {
+        alert('Error displaying data:', error);
+    }    
+}
+
+async function getAllProducts(){
+    const apiUrl = 'https://api.noroff.dev/api/v1/rainy-days';
+    const products  = await fetchData(apiUrl)
+    return products
+}
+
+export const allProducts = await getAllProducts();
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+import { allProducts } from "./fetch.js";
 import { updateCartIconDisplay } from "./cartIconDisplay.mjs";
 import './burger.js';
 import './slider.js';
+
+console.log(allProducts)
+const featuredBtn = document.querySelector('.btn-featured');
+const newBtn = document.querySelector('.btn-new');
+const header = document.querySelector('.header');
+const slideImg = document.querySelectorAll('.slide-image');
+const slideText = document.querySelectorAll('.slide-text');
 
 // change header logo to dark while hovering
 function changeTheme() {
@@ -21,7 +29,25 @@ function swtichHeaderLogo (header) {
     header.addEventListener('mouseout' ,resetTheme)
 }
 
-const header = document.querySelector('.header');
+// add click events
+featuredBtn.addEventListener('click', displayFeatureProducts);
+newBtn.addEventListener('click', displayNewProducts);
+
+// change products on display accordingly
+function displayFeatureProducts() {
+    for (let i = 0; i<slideImg.length; i++) {
+       slideImg[i].innerHTML = `<a href="jacket.html"><img src="${allProducts[i].image}" alt="${allProducts[i].description}"></a>`
+       slideText[i].innerHTML = `${allProducts[i].title}<br>$${allProducts[i].price}` 
+    }
+}
+
+function displayNewProducts() {
+    for (let i = 0; i<slideImg.length; i++) {
+        slideImg[i].innerHTML = `<a href="jacket.html"><img src="${allProducts[i+1].image}" alt="${allProducts[i+1].description}"></a>`
+        slideText[i].innerHTML = `${allProducts[i+1].title}<br>$${allProducts[i+1].price}` 
+     }
+}
 
 swtichHeaderLogo(header);
 updateCartIconDisplay();
+displayFeatureProducts()
