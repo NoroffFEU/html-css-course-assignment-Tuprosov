ad70daf195c66275ec3de7deeb888eab37c0af3a
Shahzod <shahzodwill@gmail.com>
2024-02-24 14:50:24 +0100

    change path in head

--- js/index.js
+++ js/index.js
@@ -12,14 +12,14 @@ const slideText = document.querySelectorAll('.slide-text');
 // change header logo to dark while hovering
 function changeTheme() {
     const logo = document.getElementById('logo');
-    logo.src = '../assets/images/logo-dark.png';
+    logo.src = '/assets/images/logo-dark.png';
     logo.alt = 'Rainydays logo dark';
 }
 
 // reset to default logo
 function resetTheme() {
     const logo = document.getElementById('logo');
-    logo.src = '../assets/images/logo-light.png';
+    logo.src = '/assets/images/logo-light.png';
     logo.alt = 'Rainydays logo light';
 }
 
